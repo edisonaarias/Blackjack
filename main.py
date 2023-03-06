4d1bc3c7bf1840cf7499538a3637fcc89ca4c2a9
unknown <edisonalexanderarias@gmail.com>
2023-03-05 21:45:20 -0800

    Appended the list of cards so all the cards show up in order

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ suits = ["spades", "clubs", "hearts", "diamonds"]
 ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 for suit in suits:
     for rank in ranks:
-        print([suit, ranks[0]])
+        cards.append([suit, ranks[0]])
+
+print(cards)       
