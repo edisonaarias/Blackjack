9551846c31aadb0952ec517e3b16b67a333e3f00
unknown <edisonalexanderarias@gmail.com>
2023-03-05 21:51:42 -0800

    Added pop to get one random card

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ suits = ["spades", "clubs", "hearts", "diamonds"]
 ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 for suit in suits:
     for rank in ranks:
-        cards.append([suit, ranks[0]])
+        cards.append([suit, rank])
 
-print(cards)       
+random.shuffle(cards)
+
+card = cards.pop()
+print(card)       
