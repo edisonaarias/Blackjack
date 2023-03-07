85044f44638c89c68307742004777f6b23ee69b8
unknown <edisonalexanderarias@gmail.com>
2023-03-06 23:22:32 -0800

    Removed some unwanted code

--- main.py
+++ main.py
@@ -18,17 +18,4 @@ def deal(number):
         return cards_dealt
 
 shuffle()
-cards_dealt = deal(2)
-card = cards_dealt[0]
-rank = card[1]
-
-if rank == "A":
-    value = 11
-elif rank == "J" or rank == "Q" or rank == "K":
-    value = 10
-else:
-    value = rank 
-
-rank_dict = {"rank": rank, "value": value}
-
-print(rank_dict["rank"], rank_dict["value"])     
+   
