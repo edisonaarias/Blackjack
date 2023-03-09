198e37c85f060f0678581f942de5875194f3b32b
unknown <edisonalexanderarias@gmail.com>
2023-03-08 23:44:29 -0800

    Created a card class

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import random
 
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
 class Deck:
     def __init__(self):
         self.cards = []
